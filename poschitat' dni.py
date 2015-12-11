fee12e148f8a1681266c35a73089bf10c7812950
unknown <nurbek_uulu_a@12-12.iuca.loc>
2015-12-11 17:35:39 +0600

    home work

---
+++ poschitat' dni.py
@@ -0,0 +1,11 @@
+print ("Hochew uznat' skolko projil dney ?")
+name = raw_input("name: ")
+
+print ("skolko tebe let?")
+age = int(input("age: "))
+
+days = age * 365
+minutes = age * 525948
+seconds = age * 31556926
+
+print (name, "ty projil", days, "dney", minutes, "minut", seconds , "sekund")
---
+++ samostoyatelnya.py
@@ -0,0 +1,11 @@
+print "vvedite lyuboe chislo",
+a = raw_input (">>>")
+print "vvedite eshe odno chislo"
+b = raw_input(">>>")
+if a < b:
+	print int(a) +int(b)
+if a > b:
+	print int(a) - int(b)
+if a==b:
+	print int(a) * int(b)
+	
